bf4d621c68fa55db417164c9ffd8d4fdd827656f
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-13 21:41:12 +0000

    Update get_n_elements.R

--- Chapter1/get_n_elements.R
+++ Chapter1/get_n_elements.R
@@ -3,3 +3,20 @@ get_n_elements <- function(x, ...)
 {
   UseMethod("get_n_elements")
 }
+
+# View get_n_elements
+get_n_elements
+
+# Create a data.frame method for get_n_elements
+get_n_elements.data.frame <- function(x, ...){
+    nrow(x) * ncol(x)
+}
+
+
+
+
+# Call the method on the sleep dataset
+n_elements_sleep <- get_n_elements(sleep)
+
+# View the result
+n_elements_sleep
