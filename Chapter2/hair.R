e5c936dd4d6468277bfdbdb565e04eb7f0b814d1
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-14 17:30:39 +0000

    Create hair.R

---
+++ Chapter2/hair.R
@@ -0,0 +1,11 @@
+# View the structure of hair
+str(hair)
+
+# What primitive generics are available?
+.S3PrimitiveGenerics
+
+# Does length.hairstylist exist?
+exists("length.hairstylist")
+
+# What is the length of hair?
+length(hair)
