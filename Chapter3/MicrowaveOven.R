f86cd709a25fc61663fdbc9f298c9d897d091dbc
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-15 19:03:24 +0000

    Make a new microwave oven

--- Chapter3/MicrowaveOven.R
+++ Chapter3/MicrowaveOven.R
@@ -5,3 +5,10 @@ microwave_oven_factory <- R6Class(
     power_rating_watts = 800
   )
 )
+
+
+# View the microwave_oven_factory
+microwave_oven_factory
+
+# Make a new microwave oven
+microwave_oven <- microwave_oven_factory$new()
