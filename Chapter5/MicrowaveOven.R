13b46df28a679aaf276983fb70848f2c30b9a69c
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-16 00:31:13 +0000

    Implement SmartMicrowaveOven class with DB access
    Added SmartMicrowaveOven class with database connection and cooking time retrieval.

--- Chapter5/MicrowaveOven.R
+++ Chapter5/MicrowaveOven.R
@@ -71,3 +71,37 @@ identical(a_microwave_oven$power_plug$type, cloned_microwave_oven$power_plug$typ
 
 # Check a_microwave_oven & deep_cloned_microwave_oven different 
 identical(a_microwave_oven$power_plug$type, deep_cloned_microwave_oven$power_plug$type)  
+
+# Complete the class definition
+smart_microwave_oven_factory <- R6Class(
+  "SmartMicrowaveOven",
+  inherit = microwave_oven_factory, # Specify inheritance
+  private = list(
+    # Add a field to store connection
+    conn = NULL
+  ),
+  public = list(
+    initialize = function() {
+      # Connect to the database
+      private$conn = dbConnect(RSQLite::SQLite(), "cooking-times.sqlite")
+    },
+    get_cooking_time = function(food) {
+      dbGetQuery(
+        private$conn,
+        sprintf("SELECT time_seconds FROM cooking_times WHERE food = '%s'", food)
+      )
+    },
+    finalize = function() {
+      # Print a message
+      message("Disconnecting from the cooking times database.")
+      # Disconnect from the database
+      dbDisconnect(private$conn)
+    }
+  )
+)
+
+# Create a smart microwave object
+a_smart_microwave <- smart_microwave_oven_factory$new()
+  
+# Call the get_cooking_time() method
+a_smart_microwave$get_cooking_time("soup")
